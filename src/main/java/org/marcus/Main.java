8c89dfd0463c39c000407744b67e4c4d250343af
mfbayraktar <mehmetfatih.bayraktar@ricoh.ca>
2024-02-07 13:25:38 -0500

    update v1

--- src/main/java/org/marcus/Main.java
+++ src/main/java/org/marcus/Main.java
@@ -1,7 +1,5 @@
 package org.marcus;
-
 import net.spy.memcached.MemcachedClient;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
@@ -11,24 +9,28 @@ import java.util.Map;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws IOException {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        if (args.length < 1) {
+            System.err.println("Usage: java Main <hostname:port>");
+            System.exit(1);
+        }
 
-        MemcachedClient memcachedClient = new MemcachedClient(new InetSocketAddress("localhost", 11211));
+        String[] parts = args[0].split(":");
+        if (parts.length != 2) {
+            System.err.println("Invalid argument format. Please use <hostname:port>.");
+            System.exit(1);
+        }
+
+        String hostname = parts[0];
+        int port = Integer.parseInt(parts[1]);
+
+        // Creating InetSocketAddress
+        InetSocketAddress address = new InetSocketAddress(hostname, port);
+
+        // Creating MemcachedClient with the specified address
+        MemcachedClient memcachedClient = new MemcachedClient(address);
 
         //test connection
         Map<SocketAddress, Map<String, String>> stats = memcachedClient.getStats();
         System.out.println("Connection successful!");
-
-        String key = "exampleKey";
-        String value = "exampleValue";
-        int expirationTimeInSeconds = 3600; // 1 hour
-        // Store a value (async) for one hour
-        memcachedClient.set(key, expirationTimeInSeconds, value);
-
-        // Retrieve a value.
-        String cachedValue = (String)memcachedClient.get(key);
-        System.out.println("Cached value: " + cachedValue);
-        memcachedClient.shutdown();
     }
 }

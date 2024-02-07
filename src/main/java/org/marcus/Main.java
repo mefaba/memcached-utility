ff90647d37fae5f2257916a78fe43cd64ce3c596
mfbayraktar <mehmetfatih.bayraktar@ricoh.ca>
2024-02-07 15:20:28 -0500

    update v4

--- src/main/java/org/marcus/Main.java
+++ src/main/java/org/marcus/Main.java
@@ -2,6 +2,7 @@ package org.marcus;
 import net.spy.memcached.AddrUtil;
 import net.spy.memcached.MemcachedClient;
 import java.io.IOException;
+import java.net.ConnectException;
 import java.net.InetSocketAddress;
 import java.util.Map;
 
@@ -9,7 +10,9 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         if (args.length < 1) {
-            System.err.println("Usage: java Main <hostname:port>");
+            //TODO
+            System.err.println("memcacheUtility.exe <hostname:port>");
+            System.err.println("Example: memcacheUtility.exe localhost:11211");
             System.exit(1);
         }
 
@@ -23,18 +26,19 @@ public class Main {
             InetSocketAddress address = AddrUtil.getAddresses(args[0]).get(0);
             // Creating MemcachedClient with the specified address
             MemcachedClient memcachedClient = new MemcachedClient(address);
-            Map<String, String> stats = memcachedClient.getStats().get(address);
-            int pid = Integer.parseInt(stats.get("pid"));
-            if(stats.containsKey("pid") && pid>0){
-                System.out.println("Connection to Memcached established successfully!");
-            }else{
-                System.out.println("Connection failed.");
-            }
+            // Perform a test operation (set a value)
+            memcachedClient.set("testKey", 900, "testValue");
 
+            // Perform a get operation to verify connection
+            Object myObject = memcachedClient.get("testKey");
+            System.out.println("Connection to Memcached established successfully!");
+
+            // Close the client
             memcachedClient.shutdown();
         }
-        catch (IOException e){
-            System.err.println("Failed to establish connection to Memcached: " + e.getMessage());
+        catch (Exception e){
+            System.err.println("Failed to establish connection: ");
+            e.printStackTrace();
         }
     }
 }

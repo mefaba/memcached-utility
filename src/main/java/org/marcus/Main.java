31b0d6a6bbbfa14fda58110ce434569d70c8b9c8
mfbayraktar <mehmetfatih.bayraktar@ricoh.ca>
2024-02-07 14:21:36 -0500

    clean v3

--- src/main/java/org/marcus/Main.java
+++ src/main/java/org/marcus/Main.java
@@ -5,8 +5,7 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.Map;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         if (args.length < 1) {
